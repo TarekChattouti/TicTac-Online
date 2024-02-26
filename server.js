77a96963731ed465030f068dbf07bfecc5e3bac7
Tarek Chattouti <chattoutibwin@gmail.com>
2024-02-26 17:31:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,39 +2,96 @@ const { Socket } = require('engine.io');
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-
+const cors = require('cors');  
 const app = express();
+const corsOptions = {
+  origin: 'https://tiktak-online.azurewebsites.net/',  
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = require('socket.io')(server);
 
 app.use(express.static('public'));
+
+
+let currentGames = []
+
 
 const players = {};
 let Player = 'X';
 let data = [['', '', ''], ['', '', ''], ['', '', '']];
 let gameEnd = false;
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
+
+function generateInvite() {
+
+  const inviteCode = generateUniqueIdentifier(6);
+
+  const inviteLink = `https://tiktak-online.azurewebsites.net/?invite=${inviteCode}`;
+
+
+  return inviteCode;
   
+}
+
+function generateUniqueIdentifier(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+
+io.on('connection', (socket) => {
+  const { room } = socket.handshake.query;
+  if (room) {
+    gameCode = room;
+  } 
+  else {
+    gameCode = generateInvite();
+  }
+  console.log('A user connected');
+
   io.emit('userList', Object.keys(players));
   
   players[socket.id] = Player;
-  
+  let gameFound = false;
+  function searchGame(game){
+    if (game == gameCode) {
+      gameFound=true;
+    }
+  }
+  currentGames.forEach(searchGame);
+  if (gameFound) {
+    socket.join(gameCode);
+  } else {
+    currentGames.push(gameCode);
+    socket.join(gameCode);
+  }
+  console.log(currentGames);
 
   
-  socket.emit('initialData', { Player, data });
 
-  socket.on('updateBoard', ({ Player, data }) => {
+  io.to(gameCode).emit('initialData', {gameCode, Player, data });
+
+  socket.on('updateBoard', ({gameCode, Player, data }) => {
     
-    io.emit('updateBoard', { Player, data });
+    io.to(gameCode).emit('updateBoard', { Player, data });
     checkWin();
+    
   });
 
   socket.on('resetGame', () => {
     data = [['', '', ''], ['', '', ''], ['', '', '']];
     gameEnd = false;
-    io.emit('resetGame');
+    io.to(gameCode).emit('resetGame');
   });
 
   socket.on('disconnect', () => {
