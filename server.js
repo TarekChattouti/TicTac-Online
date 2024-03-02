58e76712098cb755aa19b8f367c081161c88fe0e
Tarek Chattouti <chattoutibwin@gmail.com>
2024-03-02 03:10:49 +0100

    Updated

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 const { Socket } = require('engine.io');
 const express = require('express');
+const WebSocket = require('ws');
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');  
 const app = express();
+
+
 const corsOptions = {
-  origin: 'https://tiktak-online.azurewebsites.net/',  
+  origin: 'http://127.0.0.1:3000/',  
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
 };
+
+
+
+function generateGuestName() {
+  const adjectives = ['Happy', 'Sunny', 'Jolly', 'Radiant', 'Gleaming', 'Lively', 'Cheerful', 'Playful'];
+  const nouns = ['Explorer', 'Adventurer', 'Wanderer', 'Dreamer', 'Nomad', 'Voyager', 'Pioneer', 'Roamer'];
+
+  
+  const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+  const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+
+  
+  const guestName = `${randomAdjective} ${randomNoun}#${generateUniqueIdentifier(4)}`;
+
+  return guestName;
+}
 
 app.use(cors(corsOptions));
 
@@ -19,9 +38,22 @@ const io = require('socket.io')(server);
 app.use(express.static('public'));
 
 
-let currentGames = []
+let currentGames = {}
 
-
+let playerData = {
+  id : "",
+  name : "",
+  room : ""
+}
+let roomData = {
+  players : {
+    player0 : null,
+    player1 : null
+  },
+  isPrivate : false,
+  privatePassword : null,
+  isStarted : false
+}
 const players = {};
 let Player = 'X';
 let data = [['', '', ''], ['', '', ''], ['', '', '']];
@@ -32,7 +64,7 @@ function generateInvite() {
 
   const inviteCode = generateUniqueIdentifier(6);
 
-  const inviteLink = `https://tiktak-online.azurewebsites.net/?invite=${inviteCode}`;
+  const inviteLink = `http://127.0.0.1:3000/?invite=${inviteCode}`;
 
 
   return inviteCode;
@@ -49,54 +81,125 @@ function generateUniqueIdentifier(length) {
 }
 
 
-io.on('connection', (socket) => {
-  const { room } = socket.handshake.query;
-  if (room) {
-    gameCode = room;
-  } 
-  else {
-    gameCode = generateInvite();
-  }
-  console.log('A user connected');
 
-  io.emit('userList', Object.keys(players));
+
+io.on('connection', (socket) => {
   
-  players[socket.id] = Player;
+  const { room , url , hasJoined} = socket.handshake.query;
+  if (url == "/public.html") {
+    io.emit('gameList', { currentGames });
+    return;
+  }
+  let socketId = socket.id;
+  let playerData = {
+    id : "",
+    name : "",
+    room : ""
+  }
+  let roomData = {
+    name : "TicTac#"+generateUniqueIdentifier(4),
+    players : {
+      player0 : null,
+      player1 : null
+    },
+    isPrivate : false,
+    privatePassword : null,
+    isStarted : false
+  }
+  let error = 0;
+  
   let gameFound = false;
-  function searchGame(game){
-    if (game == gameCode) {
-      gameFound=true;
+  playerData["id"] = socketId;
+  playerData['name'] = generateGuestName();
+  let isInvited = false;
+  
+  if (room != "false" && url != "/") {
+    console.log(room);
+    if (currentGames[room]) {
+      if (currentGames[room]["players"]['player0'] != null && currentGames[room]["players"]['player1'] != null){
+        error = 6;
+        gameCode = null;
+      }
+      else {
+        
+        gameCode = room;
+        isInvited = true;
+
+        gameFound = true;
+        if (currentGames[gameCode]['players']['player0'] == null) {
+          currentGames[gameCode]['players']['player0'] = socketId;
+          
+        } else {
+
+          currentGames[gameCode]['players']['player1'] = socketId;
+        }
+      }
+    }
+    else {
+      error = 3;
+      gameCode = null;
     }
   }
-  currentGames.forEach(searchGame);
-  if (gameFound) {
-    socket.join(gameCode);
-  } else {
-    currentGames.push(gameCode);
-    socket.join(gameCode);
+  
+  else {
+    gameFound=true;
+    gameCode = generateInvite();
   }
-  console.log(currentGames);
+  if (gameCode != null && !isInvited) {
+    currentGames[gameCode] = roomData;
+  }
+  playerData["room"] = gameCode;
+  players[socketId] = playerData;
+
+  console.log('A user connected',players[socketId]);
 
   
-
-  io.to(gameCode).emit('initialData', {gameCode, Player, data });
+  
+  
+  socket.join(gameCode);
+  
+  gameData = currentGames[gameCode];
+  io.to(gameCode).emit('initialData', {gameData, error , playerData , Player, data });
 
   socket.on('updateBoard', ({gameCode, Player, data }) => {
+    currentGames[gameCode]["isStarted"] == true;
     
     io.to(gameCode).emit('updateBoard', { Player, data });
-    checkWin();
     
   });
 
   socket.on('resetGame', () => {
+    socketId = socket.id;
     data = [['', '', ''], ['', '', ''], ['', '', '']];
     gameEnd = false;
-    io.to(gameCode).emit('resetGame');
+    io.to(players[socketId]["room"]).emit('resetGame');
   });
 
   socket.on('disconnect', () => {
-    delete players[socket.id];
-    console.log('A user disconnected');
+    
+    let disconnect = true;
+    socketId = socket.id;
+    if (players[socketId]["room"] != null){
+      if (currentGames[players[socketId]["room"]]["players"]["player0"] == null && currentGames[players[socketId]["room"]]["players"]["player1"] == null) {
+        delete currentGames[players[socketId]["room"]];
+      }else {
+        if (currentGames[players[socketId]["room"]]["players"]["player0"] == socketId) {
+          currentGames[players[socketId]["room"]]["players"]["player0"] = null;
+        }else {
+          currentGames[players[socketId]["room"]]["players"]["player1"] = null;
+        }
+        if (true) {
+          io.to(players[socketId]["room"]).emit('disconnected', { disconnect });
+
+        }
+      }
+      
+    }
+
+    
+    delete players[socketId];
+    
+    console.log('A user disconnected',socketId);
   });
 });
 
@@ -104,6 +207,12 @@ const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+
 
 function checkWin() {
   // Your existing checkWin logic here...
